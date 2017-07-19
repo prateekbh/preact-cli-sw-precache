18c6d772d254564142fc9a16e21b682b4ecae3e6
Prateek Bhatnagar <prateek89born@gmail.com>
2017-07-19 16:12:02 +0530

    adding first cut plugin

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const SwPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+const preactCliPrecachePlugin = (config, swPrecacheOptions) => {
+  if (!config) {
+    throw Error('Webpack config not given!');
+  }
+  if (!swPrecacheOptions) {
+    throw Error('swPrecache options not given!');
+  }
+  const plugins = config.plugins;
+  for (let pluginIndex = 0; pluginIndex < plugins.length; pluginIndex++) {
+    const plugin = plugins[pluginIndex];
+    if (plugin instanceof SwPrecacheWebpackPlugin){
+      plugin.options = swPrecacheOptions;
+    }
+
+  }
+  return config;
+};
+
+module.exports = preactCliPrecachePlugin;
