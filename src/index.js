7b34439b57217ee1faf05befdb027560e094a635
Prateek Bhatnagar <prateek89born@gmail.com>
2017-08-25 21:34:36 +0530

    lint fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
-
 const preactCliPrecachePlugin = (config, swPrecacheOptions) => {
     if (!config) {
         throw Error('Webpack config not given!')
@@ -7,7 +5,7 @@ const preactCliPrecachePlugin = (config, swPrecacheOptions) => {
     if (!swPrecacheOptions) {
         throw Error('swPrecache options not given!')
     }
-    const plugins = config.plugins;
+    const plugins = config.plugins
     for (let pluginIndex = 0; pluginIndex < plugins.length; pluginIndex++) {
         const plugin = plugins[pluginIndex]
         if (plugin && plugin.options && plugin.options.cacheId){
